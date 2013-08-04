bc91cb07a26efa08e6877eb03fe3fea35c261b5a
nduchene <nathan@nduchene.com>
2013-08-04 10:43:52 -0400

    01 - Base App - No SignalR

---
+++ pghdotnetdemo/Code/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pghdotnetdemo.Code
+{
+    public class Game
+    {
+        public int ID { get; set; }
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+
+        public Game()
+        {
+            this.HomeScore = 0;
+            this.AwayScore = 0;
+        }
+    }
+}
---
+++ pghdotnetdemo/Code/GameRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pghdotnetdemo.Code
+{
+    public static class GameRepository
+    {
+        private static Lazy<List<Game>> _games = new Lazy<List<Game>>();
+
+        public static List<Game> Games 
+        {
+            get
+            {
+                return _games.Value;
+            }
+        }
+    }
+}
